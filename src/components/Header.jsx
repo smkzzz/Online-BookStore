4dab871d81641d7868727afe476cdb0dc8328b3b
smkzzz <93688913+smkzzz@users.noreply.github.com>
2023-11-08 15:32:13 +0000

    test comm

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,16 @@
 import React from "react";
 import { useState } from "react";
 import { TbLayoutSidebarLeftCollapse, TbLayoutSidebarRightCollapse } from "react-icons/tb";
-function Header({collapse,setCollapse}) {
-  
+function Header({ collapse, setCollapse }) {
+
 
   const handleSide = () => setCollapse((prev) => !prev)
   return (
-  
-    <div className="p-5 text-white bg-slate-900 " ><span className="cursor-pointer" onClick={handleSide}>{collapse ? <TbLayoutSidebarRightCollapse /> : <TbLayoutSidebarLeftCollapse/>}</span></div>
-    
+
+    <div className="p-5 text-white bg-slate-900 " ><span className="cursor-pointer" onClick={handleSide}>{collapse ? <TbLayoutSidebarRightCollapse /> : <TbLayoutSidebarLeftCollapse />}</span>
+      <a>asdsa</a>
+    </div>
+
   );
 }
 
